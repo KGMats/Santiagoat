a1fbf705646166a951a24052bbdc5cc2d4e9a904
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2025-10-27 17:37:22 -0300

    Fixed grasp

--- Meta-Heuristics/grasp.h
+++ Meta-Heuristics/grasp.h
@@ -6,8 +6,8 @@
 #define GRAFOS_GRASP_H
 #include "Graph.h"
 
-uint64_t GRASP(Graph* graph, const unsigned int max_iterations,
-        const int seed, const bool randomGreedyFunction(const Graph*,uint64_t *result, const uint64_t),
-        const uint64_t localSearchFunction(const Graph *graph, uint64_t *bestSolution, uint64_t nActiveNodes));
+uint64_t GRASP(Graph *graph, const unsigned int max_iterations,
+               const int seed, const bool randomGreedyFunction(const Graph *, uint64_t *result, const uint64_t),
+               const uint64_t localSearchFunction(const Graph *graph, uint64_t *bestSolution, uint64_t nActiveNodes));
 
 #endif //GRAFOS_GRASP_H
