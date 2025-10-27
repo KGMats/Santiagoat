9e9edc174ed2626122511937163df0e1385cba4e
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2025-10-27 17:37:14 -0300

    Fixed grasp

--- Meta-Heuristics/grasp.c
+++ Meta-Heuristics/grasp.c
@@ -1,33 +1,42 @@
 #include <math.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "Graph.h"
 
-uint64_t GRASP(Graph* graph, uint64_t initialActiveNodes,
-        const unsigned int max_iterations,
-        const int seed, const bool randomGreedyFunction(Graph*, uint64_t),
-        const uint64_t localSearchFunction(Graph*, uint64_t))
-{
+uint64_t GRASP(Graph *graph, unsigned int max_iterations,
+               const int seed, const bool randomGreedyFunction(const Graph *, uint64_t *result, const uint64_t),
+               const uint64_t localSearchFunction(const Graph *graph, uint64_t *bestSolution, uint64_t nActiveNodes)) {
         srand(seed);
+        float multiplier = 0.5f;
 
-        uint64_t best_solution = 0; // 0 indica que não tem solucao
+        uint64_t best_solution = 99999; // 0 indica que não tem solucao
+        uint64_t *result = malloc(sizeof(uint64_t) * graph->n_nodes);
 
-
-        for (int i = 0; i < max_iterations; i++) {
-                if (initialActiveNodes == 0) {
-                        break;
+        while (max_iterations != 0) {
+                max_iterations--;
+                deactivateAll(graph);
+                uint64_t solution_size = graph->n_nodes * multiplier;
+                bool is_solution = randomGreedyFunction(graph, result, solution_size);
+                if (!is_solution) {
+                        multiplier += 0.05f;
+                        solution_size = graph->n_nodes * multiplier;
+                        deactivateAll(graph);
+                        is_solution = randomGreedyFunction(graph, result, solution_size);
                 }
 
-                bool is_solution = randomGreedyFunction(graph, initialActiveNodes);
-                if (is_solution) {
-                        best_solution = initialActiveNodes;
-                        uint64_t solution = localSearchFunction(graph, initialActiveNodes);
-                        best_solution = (solution > best_solution) ? solution : best_solution;
-                        initialActiveNodes--;
+                if (!is_solution) {
+                        continue;
                 }
-
+                best_solution = (best_solution < solution_size) ? best_solution : solution_size;
+                printf("%d\n", best_solution);
+                solution_size = localSearchFunction(graph, result, best_solution);
+                best_solution = (best_solution < solution_size) ? best_solution : solution_size;
         }
+
+
+        free(result);
         return best_solution;
 }
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <time.h>
 
 #include "Algorithms.h"
+#include "fullRandomHeuristic.h"
 #include "Solutions.h"
 #include "Graph.h"
 
@@ -9,15 +10,16 @@
 int main(const int argc, char** argv) {
     const char *filename = "../graphs/facebook_combined.txt";
 
-    srand(time(NULL));
-
     if (argc > 1) {
          filename = argv[1];
     }
     Graph* graph = createGraphFromFilename(filename);
 
-    testLocalSearch(graph, maximumDegreeHeuristic, hillClimbSimple);
+    // testLocalSearch(graph, bridgeHeuristic, hillClimbSimple);
     // testHeuristics(graph, bridgeHeuristic);
+    int sol = GRASP(graph, 10, time(NULL), fullRandomHeuristic, hillClimbSimple);
+    printf("%d\n", sol);
+
     freeGraph(graph);
     return 0;
 }
